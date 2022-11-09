015e7f9abdbecf0505079b5e93e4a714bd298396
willi4en <46721116+willi4en@users.noreply.github.com>
2022-11-09 11:08:25 -0500

    AlternaCanvas

---
+++ AlternaCanvas/alternacanvas/src/components/mainpage.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Badge from 'react-bootstrap/Badge';
+
+class MainMenu extends Component {
+
+    styleBackground = {
+        backgroundColor: 'white',
+        height: '100vh'
+    };
+
+    render() {
+        return (
+            <div style={this.styleBackground}>
+
+            </div>
+        )
+    }
+}
+
+export default MainMenu;
